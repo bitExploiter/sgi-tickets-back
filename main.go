de0465678245b0f64d8c2a0679ce5ff77bd63071
AlvaroAlvarezR <alvaroalvarezr08@gmail.com>
2026-03-12 09:47:08 -0500

    feat: Update CORS configuration to allow requests from localhost:3000

--- main.go
+++ main.go
@@ -70,7 +70,7 @@ func main() {
 	}))
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173",
+		AllowOrigins:     "http://localhost:3000",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 		AllowCredentials: true,
